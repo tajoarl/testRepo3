625d2bed272a00afae0d6a5b219ee837d1eb5172
jaden.j <jaden.j@kakaocorp.com>
2017-07-28 16:05:33 +0900

    default setting

---
+++ config/webpack.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+    entry: '/app/index.js',
+    output: {
+        path: __dirname + '/public/',
+        publicPath: '/',
+        filename: 'bundle.js'
+    },
+    devServer: {
+        inline: true,
+        host: '0.0.0.0',
+        contentBase: '/public/',
+        disableHostCheck: true
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                loader: 'babel-loader',
+                exclude: '/node_modules/',
+                query: {
+                    presets: ['es2015', 'react']
+                }
+            }
+        ]
+    }
+}
